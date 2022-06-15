de793737096ffc9ee65aca76fd9ef598589e09eb
BekirSitki <sitkibekiryilmaz@gmail.com>
2022-06-15 08:49:11 +0300

    Düzenlemeler tamamlandı.

--- proje/models.py
+++ proje/models.py
@@ -1,10 +1,10 @@
 import django
 from django.db import models
 from datetime import date
-
+from django import utils
 # Create your models here.
 
 class Notlar(models.Model):
     baslik = models.CharField(max_length=30)
     icerik = models.CharField(max_length=255)
-    tarih  = models.DateField(default=date.today())
+    tarih  = models.DateField(default=django.utils.timezone.now)
